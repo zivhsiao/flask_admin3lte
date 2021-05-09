8cf197d1ce16c4b1aa997b03b0e94f5a5f3ec3b3
Patrick <jenfu.developer@gmail.com>
2021-05-09 11:56:49 +0800

    update admin

--- app/base/routes.py
+++ app/base/routes.py
@@ -18,6 +18,8 @@ from app.base.models import User, Question, QuestionType
 
 from app.base.util import verify_pass
 
+import json
+
 @blueprint.route('/')
 def route_default():
     return redirect(url_for('base_blueprint.login'))
@@ -93,30 +95,125 @@ def logout():
     logout_user()
     return redirect(url_for('base_blueprint.login'))
 
-## question 
-@blueprint.route('/question-type-new')
-def question_type_new():
-    return render_template('question/question-type-new.html', segment='question-type-new')
-
+## question type
 @blueprint.route('/question-type-edit')
 def question_type_edit():
+    if not current_user.is_authenticated:
+        return redirect('/login')
     question_types = QuestionType.query.all()
     return render_template('question/question-type-edit.html', question_types=question_types, segment='question-type-edit')
 
+@blueprint.route('/delete-type',  methods=["POST", "GET"])
+def delete_type():
+    if not current_user.is_authenticated:
+        return 'Forbidden', 403
+    question_id = request.form.get('id')
+    query = QuestionType.query.filter_by(id = question_id).first()
+    db.session.delete(query)
+    db.session.commit()
+    return '', 200  
 
-@blueprint.route('/question-new')
-def question_type_edit():
-    return render_template('question/question-new.html', question_types=question_types, segment='question-new')
+@blueprint.route('/update-type',  methods=["POST", "GET"])
+def update_type():
+    if not current_user.is_authenticated:
+        return 'Forbidden', 403
+    question_id = request.form.get('id')
+    query = QuestionType.query.filter_by(id = question_id).first()
+   
+    return jsonify({"id": f"{query.id}", 
+                    "type_name":f"{query.type_name}"
+                    })
 
+@blueprint.route('/update-type-action',  methods=["POST", "GET"])
+def update_type_action():
+    if not current_user.is_authenticated:
+        return 'Forbidden', 403
+    question_id = request.form.get('id')
+    type_name = request.form.get('type_name')
+    if question_id == '':
+        # 新增資料
+        q = QuestionType(type_name = type_name)
+        db.session.add(q)             
+        db.session.commit()
+        
+    else:
+        # 更新資料    
+        q = QuestionType.query.filter_by(id = question_id).first()
+        q.type_name = type_name
+        db.session.commit()
+    
+    return redirect('/question-type-edit')
 
-@blueprint.route('/question-edit')
-def question_type_edit():
-    questions = Question.query.all()
-    return render_template('question/question-edit.html', questions=questions, segment='question-edit')
+# question
+@blueprint.route('/delete-question',  methods=["POST", "GET"])
+def delete_question():
+    if not current_user.is_authenticated:
+        return 'Forbidden', 403    
+    question_id = request.form.get('id')
+    query = Question.query.filter_by(id = question_id).first()
+    db.session.delete(query)
+    db.session.commit()
+    return '', 200  
+
+@blueprint.route('/update-question',  methods=["POST", "GET"])
+def update_question():
+    if not current_user.is_authenticated:
+        return 'Forbidden', 403    
+    question_id = request.form.get('id')
+    query = Question.query.filter_by(id = question_id).first()
+   
+    return jsonify({"id": f"{query.id}", 
+                    "question_type":f"{query.question_type}", 
+                    "question": f"{query.question}",
+                    "answer": f"{query.answer}",
+                    "keyword": f"{query.keyword}"
+                    })
+
+@blueprint.route('/update-question-action',  methods=["POST", "GET"])
+def update_question_action():
+    if not current_user.is_authenticated:
+        return 'Forbidden', 403    
+    question_id = request.form.get('id')
+    question_type = request.form.get('question_type')
+    question = request.form.get('question')
+    answer = request.form.get('answer')
+    keyword = request.form.get('keyword')
+    if question_id == '':
+        # 新增資料
+        q = Question(question_type = question_type,
+                     question = question,
+                     answer = answer,
+                     keyword = keyword
+                    )
+        db.session.add(q)             
+        db.session.commit()
+        
+    else:
+        # 更新資料    
+        q = Question.query.filter_by(id = question_id).first()
+        q.question_type = question_type
+        q.question = question
+        q.answer = answer
+        q.keyword = keyword
+        db.session.commit()
+    
+    return redirect('/qna-edit')
+
+# question
+@blueprint.route('/qna-edit')
+def question_edit():
+    if not current_user.is_authenticated:
+        return redirect('/login')
+    questions = db.session.query(Question, QuestionType).filter(Question.question_type == QuestionType.id).order_by(Question.id.desc()).all()
+    question_type = QuestionType.query.all()    
+    return render_template('question/qna-edit.html', 
+        questions = questions,
+        question_type = question_type,  
+        segment='qna-edit'
+    )
 
 
 ## Errors
-
 @login_manager.unauthorized_handler
 def unauthorized_handler():
     return render_template('page-403.html'), 403
